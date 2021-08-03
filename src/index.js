64f8470f436760b7c2d51445447c67db60d2852a
nurbrun <the.nurbrun@gmail.com>
2021-08-03 03:52:29 -0400

    added back BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDoContainer from './functionBased/components/ToDoContainer.js';
 import './functionBased/App.css'
-// import { BrowserRouter as Router } from "react-router-dom"
-import { HashRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+// import { HashRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ToDoContainer />
     </Router>,
   document.getElementById('root')
