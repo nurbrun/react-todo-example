c91bc0c2e34aaad4f6482b02f5b71af390745e91
nurbrun <the.nurbrun@gmail.com>
2021-08-03 03:26:02 -0400

    added hasrouter for gh pages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDoContainer from './functionBased/components/ToDoContainer.js';
 import './functionBased/App.css'
-import { BrowserRouter as Router } from "react-router-dom"
-
+// import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter as Router } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
